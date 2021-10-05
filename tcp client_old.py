37d9d7b9ba481d9a9d2e2a3fc970c918263a4d95
A Poor Cat <82797313+poor-cat@users.noreply.github.com>
2021-10-05 09:33:29 +0530

    Update tcp client_old.py
    i made some major changes in the client to make it faster :) #hacktoberfest2021

--- tcp client_old.py
+++ tcp client_old.py
@@ -6,13 +6,16 @@ target_port = 80
 # create a socket object 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect the client 
-client.connect({target_host, target_port})
+client.connect((hostname, port))
 
-# send some data 
-client.send("GET /HTTP/1.1\r\Host: google.com\r\n\r\n")
+client.sendall('hola amigos\n');
 
-# receive some data
-response = client.recv(4096)
-
-print("response")
+data = '' 
+while (data != '/exit'):
+  try:
+    data = raw_input('>> ')
+    if (data != '/exit'):
+      response = client.recv(4096);
+      print(response);
+  except EOFError:
+    break
